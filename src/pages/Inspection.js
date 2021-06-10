82b489b7ae5389b217c3ee527acfb2c2b4280cec
sandrahfiestas <shfiestas@gmail.com>
2021-06-10 11:46:35 -0500

    show component create case

---
+++ src/pages/Inspection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewCase from '../components/NewCase';
+
+function Inspection() {
+  return (
+    <>
+      <h1>Header</h1>
+      <NewCase />
+      {/* <h1>Footer</h1> */}
+    </>
+  );
+}
+
+export default Inspection;
